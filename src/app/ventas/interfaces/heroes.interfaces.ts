6ea2cb73310fbc94ed95bc7f2d2e078e78538d46
Nicolas <bernalmedina.n@gmail.com>
2021-12-17 20:42:31 -0500

    Pipe Personalizados 2

---
+++ src/app/ventas/interfaces/heroes.interfaces.ts
@@ -0,0 +1,10 @@
+
+export enum Color {
+    Rojo, Verde, Negro, Azul
+} 
+
+export interface Heroe {
+    nombre: string;
+    vuela: boolean;
+    color: Color;
+} 
--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import { Heroe, Color } from '../../interfaces/heroes.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -9,7 +10,42 @@ import { Component} from '@angular/core';
 export class OrdenarComponent {
 
   onOff: boolean = true;
+  heroes: Heroe[] = [
+    { 
+      nombre: "Superman",
+      vuela: true,
+      color: Color.Azul
+    },
+    { 
+      nombre: "Batman",
+      vuela: false,
+      color: Color.Negro
+    },
+    { 
+      nombre: "Robin",
+      vuela: false,
+      color: Color.Rojo
+    },
+    { 
+      nombre: "Linterna Verde",
+      vuela: true,
+      color: Color.Verde
+    },
+    { 
+      nombre: "Mujer Maravilla",
+      vuela: true,
+      color: Color.Azul
+    }
+
+
+  ];
+  
   cambiarMayuscula(){
     this.onOff = !this.onOff ;
   }
+
+  cambiar(){
+    
+  }
+
 }
